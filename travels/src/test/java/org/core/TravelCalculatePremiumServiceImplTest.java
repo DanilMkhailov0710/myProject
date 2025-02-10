b935eeacc2c44015f57e06fc43df21399f6a77ff
DanilMkhailov0710 <dania002@yandex.ru>
2025-02-10 23:41:29 +0300

    Checking run with mistakes

--- travels/src/test/java/org/core/TravelCalculatePremiumServiceImplTest.java
+++ travels/src/test/java/org/core/TravelCalculatePremiumServiceImplTest.java
@@ -2,11 +2,13 @@ package org.core;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
     public void deleteMe() {
-
+        assertEquals(false, true);
     }
 
 }
